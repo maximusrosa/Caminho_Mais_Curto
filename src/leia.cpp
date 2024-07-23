8b943e0b2dbd2a497d60ced49de9fc6874a6984e
Marcus Ritt <marcus.ritt@inf.ufrgs.br>
2024-07-23 13:04:27 -0300

    Revise codes.

---
+++ src/leia.cpp
@@ -0,0 +1,24 @@
+// Leitura (exemplo)
+void read_dimacs(std::istream& in, unsigned& n, unsigned& m, MyGraph& a) {
+  std::string line="", dummy;
+  while (line.substr(0,4) != "p sp")
+    getline(in,line);
+ 
+  // (1) get nodes and edges
+  std::stringstream linestr;
+  linestr.str(line);
+  linestr >> dummy >> dummy >> n >> m;
+  a.resize(n);
+  unsigned i=0;
+  while (i<m) {
+    getline(in,line);
+    if (line.substr(0,2) == "a ") {
+      std::stringstream arc(line);
+      unsigned u,v,w;
+      char ac;
+      arc >> ac >> u >> v >> w;
+      // processar arco (u,v) com peso w
+      i++;
+    }
+  }
+}
