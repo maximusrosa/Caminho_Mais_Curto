cb4680fc4393c7d3b9b31a1ab7f309d208ffdfcd
Marcus Ritt <98472489+mrpritt@users.noreply.github.com>
2025-03-12 12:35:05 -0300

    Update leia.cpp

--- src/leia.cpp
+++ src/leia.cpp
@@ -10,15 +10,14 @@ void read_dimacs(std::istream& in, unsigned& n, unsigned& m, MyGraph& a) {
   linestr >> dummy >> dummy >> n >> m;
   a.resize(n);
   unsigned i=0;
-  while (i<m) {
+  while (i++ < m) {
     getline(in,line);
     if (line.substr(0,2) == "a ") {
       std::stringstream arc(line);
       unsigned u,v,w;
       char ac;
       arc >> ac >> u >> v >> w;
-      // processar arco (u,v) com peso w
-      i++;
+      // processa arco (u,v) com peso w
     }
   }
 }
