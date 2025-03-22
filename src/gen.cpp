e6162e847424c75f229672ac94e7d521e18353a7
Maximus Rosa <maxborgesrosa@gmail.com>
2025-03-22 01:17:41 -0300

    Initial Refactoring

--- src/gen.cpp
+++ src/gen.cpp
@@ -2,65 +2,87 @@
  * \file gen.cpp
  *   \author Marcus Ritt <mrpritt@inf.ufrgs.br>
  *   \date Time-stamp: <2024-07-23 13:03:31 ritt>
+ * 
+ *  Refactored by Maximus Rosa <mbrosa@inf.ufrgs.br>
  */
 
-#include <cassert>
-#include <iostream>
-using namespace std;
-
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/connected_components.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-using namespace boost;
-
-// information stored in vertices
-struct VertexInformation {
-  unsigned component;
-};
-
-// information stored in edges
-struct EdgeInformation {
-  unsigned weight;
-};
-
-const unsigned maxweight = 1000;
-
-// graph is an adjacency list represented by vectors
-typedef adjacency_list<vecS, vecS, directedS, VertexInformation, EdgeInformation> Graph;
-typedef graph_traits<Graph>::vertex_descriptor Node;
-typedef graph_traits<Graph>::edge_descriptor Edge;
-
-int main(int argc, char *argv[]) {
-  assert(argc == 3);
-  unsigned n = atoi(argv[1]);
-  double p = atof(argv[2]);
-
-  srand48(time(0));
-
-  // (1) generate random graph
-  Graph g;
-
-  for (unsigned i = 0; i < n; i++)
-    add_vertex(g);
-
-  for (unsigned i = 0; i < n; i++)
-    for (unsigned j = 0; j < n; j++)
-      if (i != j && drand48() < p) {
-        Edge e = add_edge(i, j, g).first;
-        g[e].weight = lrand48() % maxweight;
-      }
-
-  // (2) print example path
-  unsigned src = lrand48() % num_vertices(g);
-  unsigned dst = lrand48() % num_vertices(g);
-
-  vector<unsigned> dist(n);
-  vector<unsigned> pred(n);
-  dijkstra_shortest_paths(g, src, weight_map(get(&EdgeInformation::weight, g)).distance_map(&dist[0]).predecessor_map(&pred[0]));
-  cerr << "Distance between " << src + 1 << " and " << dst + 1 << " is " << dist[dst] << endl;
-
-  // (3) print out in DIMACS challenge format
-  cout << "p sp " << num_vertices(g) << " " << num_edges(g) << endl;
-  for (auto [eb, ee] = edges(g); eb != ee; eb++)
-    cout << "a " << source(*eb, g) + 1 << " " << target(*eb, g) + 1 << " " << g[*eb].weight << endl;
-}
+ #include <cassert>
+ #include <iostream>
+ #include <vector>
+ #include <cstdlib>
+ #include <ctime>
+ #include <boost/graph/adjacency_list.hpp>
+ #include <boost/graph/dijkstra_shortest_paths.hpp>
+ using namespace boost;
+ using namespace std;
+ 
+ struct VertexInformation {
+     unsigned component;
+ };
+ 
+ struct EdgeInformation {
+     unsigned weight;
+ };
+ 
+ const unsigned maxweight = 1000;
+ typedef adjacency_list<vecS, vecS, directedS, VertexInformation, EdgeInformation> Graph;
+ typedef graph_traits<Graph>::vertex_descriptor Node;
+ typedef graph_traits<Graph>::edge_descriptor Edge;
+ 
+ graph_traits<Graph>::vertices_size_type get_random_vertex(const Graph& g) {
+     return lrand48() % num_vertices(g);
+ }
+ 
+ void generate_random_graph(Graph& g, unsigned n, double p) {
+     for (unsigned i = 0; i < n; i++) {
+         add_vertex(g);
+     }
+     
+     for (unsigned i = 0; i < n; i++) {
+         for (unsigned j = 0; j < n; j++) {
+             if (i != j && drand48() < p) {
+                 Edge e = add_edge(i, j, g).first;
+                 g[e].weight = lrand48() % maxweight;
+             }
+         }
+     }
+ }
+ 
+ void compute_shortest_path(const Graph& g, unsigned src, unsigned dst) {
+     vector<unsigned> dist(num_vertices(g));
+     vector<unsigned> pred(num_vertices(g));
+     dijkstra_shortest_paths(
+         g, src,
+         weight_map(get(&EdgeInformation::weight, g))
+             .distance_map(&dist[0])
+             .predecessor_map(&pred[0])
+     );
+     cerr << "Distance between " << src + 1 << " and " << dst + 1 << " is " << dist[dst] << endl;
+ }
+ 
+ void print_graph_dimacs(const Graph& g) {
+     cout << "p sp " << num_vertices(g) << " " << num_edges(g) << endl;
+     for (auto [eb, ee] = edges(g); eb != ee; eb++) {
+         cout << "a " << source(*eb, g) + 1 << " " << target(*eb, g) + 1 << " " << g[*eb].weight << endl;
+     }
+ }
+ 
+ int main(int argc, char *argv[]) {
+     assert(argc == 3);
+     unsigned n = atoi(argv[1]);
+     double p = atof(argv[2]);
+     
+     srand48(time(0));
+ 
+     Graph g;
+     generate_random_graph(g, n, p);
+     
+     unsigned src = get_random_vertex(g);
+     unsigned dst = get_random_vertex(g);
+     compute_shortest_path(g, src, dst);
+     
+     print_graph_dimacs(g);
+     
+     return 0;
+ }
+ 
--- src/leia.cpp
+++ src/leia.cpp
@@ -1,23 +1,68 @@
-// Leitura (exemplo)
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <utility> // for std::pair
+#include <unordered_map>
+
+// Define MyGraph as an adjacency list using unordered_map
+using MyGraph = std::vector<std::unordered_map<unsigned, unsigned>>;
+
+
+// Resize the graph to accommodate 'n' vertices
+void resize(MyGraph& graph, unsigned n) {
+    graph.resize(n);
+}
+
+// Add an edge from u to v with weight w
+void add_edge(MyGraph& graph, unsigned u, unsigned v, unsigned w) {
+    graph[u][v] = w; // Armazena o peso da aresta de u para v
+}
+
 void read_dimacs(std::istream& in, unsigned& n, unsigned& m, MyGraph& a) {
-  std::string line="", dummy;
-  while (line.substr(0,4) != "p sp")
-    getline(in,line);
- 
-  // (1) get nodes and edges
-  std::stringstream linestr;
-  linestr.str(line);
-  linestr >> dummy >> dummy >> n >> m;
-  a.resize(n);
-  unsigned i=0;
-  while (i++ < m) {
-    getline(in,line);
-    if (line.substr(0,2) == "a ") {
-      std::stringstream arc(line);
-      unsigned u,v,w;
-      char ac;
-      arc >> ac >> u >> v >> w;
-      // processa arco (u,v) com peso w
+    std::string line;
+    std::string dummy;
+    while (line.substr(0, 4) != "p sp") {
+        std::getline(in, line);
     }
-  }
+
+    // (1) Get nodes and edges
+    std::stringstream linestr(line);
+    linestr >> dummy >> dummy >> n >> m;
+    resize(a, n); // Resize the graph to hold 'n' vertices
+
+    unsigned i = 0;
+    while (i++ < m) {
+        std::getline(in, line);
+        if (line.substr(0, 2) == "a ") {
+            std::stringstream arc(line);
+            unsigned u, v, w;
+            char ac;
+            arc >> ac >> u >> v >> w;
+            u--; v--; // Convert to 0-indexed (if DIMACS uses 1-indexed vertices)
+            add_edge(a, u, v, w); // Add the edge to the graph
+        }
+    }
+}
+
+int main() {
+    // Example usage
+    std::istringstream input(
+        "p sp 3 2\n"
+        "a 1 2 10\n"
+        "a 2 3 20\n"
+    );
+
+    unsigned n, m;
+    MyGraph graph;
+    read_dimacs(input, n, m, graph);
+
+    // Print the graph (for verification)
+    for (unsigned u = 0; u < n; ++u) {
+        for (const auto& [v, w] : graph[u]) {
+            std::cout << "Edge: " << u + 1 << " -> " << v + 1 << " (weight: " << w << ")\n";
+        }
+    }
+
+    return 0;
 }
